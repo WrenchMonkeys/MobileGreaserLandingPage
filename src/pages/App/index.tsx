6fcf53e60b562c21143883347216174dbdf42bdd
Eric Gomez <eric_gomez_1@hotmail.com>
2023-02-25 22:02:43 -0600

    more fixes

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -8,31 +8,31 @@ import GooglePlayButton from "../../components/GooglePlayButton";
 
 function Index() {
   return (
-    <Container className="p-sm-0">
+    <Container>
       {/* TODO: refactor each hero into an HOC component */}
       <Divider/>
-      <div className="p-3 d-flex flex-row">
-        <div className="w-50">
-          <h1>on-demand mobile mechanics</h1>
+      <div className="d-flex flex-row">
+        <div className="p-3 d-flex flex-column w-50">
+          <h1>On Demand Mobile Mechanics</h1>
           <p className="text-secondary">Mobile Greaser helps you service your vehicle any time, any location, at your convenience.</p>
           <GooglePlayButton/>
         </div>
-        <div className="w-50 align-middle d-flex align-items-center">
+        <div className="p-3 d-flex align-middle d-flex align-items-center w-50">
           <Image className="mx-auto img-fluid" src="https://via.placeholder.com/300"/>
         </div>
       </div>
       <Divider/>
-      <div className="p-3 d-flex flex-column">
+      <div className="d-flex flex-column">
         <div className="p-3 d-flex flex-column justify-content-center align-items-center">
           <h2>Save time at the shop</h2>
           <p>Join our clients to save time and money on your vehicle maintenance</p>
         </div>
         <div className="p-3 d-flex">
-          <Image className="mx-auto img-fluid w-75 h-auto" src="https://via.placeholder.com/1200x300"/>
+          <Image className="mx-auto img-fluid w-100 h-auto" src="https://via.placeholder.com/1200x300"/>
         </div>
       </div>
       <Divider/>
-      <div className="p-3 d-flex flex-row-reverse">
+      <div className="d-flex flex-row-reverse">
         <div className="p-3 d-flex flex-column justify-content-center w-50">
           <h2>Get a complete service report</h2>
           <p>With every service request, we will include a free 50 point inspection report.</p>
@@ -42,7 +42,7 @@ function Index() {
         </div>
       </div>
       <Divider/>
-      <div className="p-3 d-flex flex-row">
+      <div className="d-flex flex-row">
         <div className="p-3 d-flex flex-column justify-content-center w-50">
           <h2>Drive with piece of mind</h2>
           <p>With our 5000 mile warranty on all services we offer, you never have to worry about improper installation or faulty parts again</p>
@@ -52,7 +52,7 @@ function Index() {
         </div>
       </div>
       <Divider/>
-      <div className="p-3 d-flex flex-row-reverse">
+      <div className="d-flex flex-row-reverse">
         <div className="p-3 d-flex flex-column justify-content-center w-50">
           <h2>Simple and affordable Pricing</h2>
           <p>With our flat rate pricing you never have to worry about being up-sold at the shop again.</p>
@@ -63,17 +63,17 @@ function Index() {
         </div>
       </div>
       <Divider/>
-      <footer className="p-3 d-flex flex-lg-row flex-column justify-content-lg-around">
-        <section className="d-flex flex-column justify-content-lg-between">
+      <footer className="d-flex flex-lg-row flex-column justify-content-lg-around">
+        <section className="p-2 d-flex flex-column justify-content-lg-between">
           <h5 className="fw-bold">Mobile Greaser</h5>
           <p>Copyright © 2023 Mobile Greaser, Inc. All rights reserved</p>
         </section>
-        <section className="d-flex flex-column justify-content-lg-between">
+        <section className="p-2 d-flex flex-column justify-content-lg-between">
           <h5 className="fw-bold">Company</h5>
           <p><a className="text-secondary" href="https://twitter.com">Twitter</a></p>
           <p><a className="text-secondary" href="mailto:egomez@mobilegreaser.com">team@mobilegreaser.com</a></p>
         </section>
-        <section className="d-flex flex-column justify-content-lg-between">
+        <section className="p-2 d-flex flex-column justify-content-lg-between">
           <h5 className="fw-bold">Legal</h5>
           <p><a className="text-secondary" href="https://google.com">Terms of Service</a></p>
           <p><a className="text-secondary" href="https://google.com">Privacy Policy</a></p>
