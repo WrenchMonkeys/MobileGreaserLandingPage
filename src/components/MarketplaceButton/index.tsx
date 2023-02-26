62290d8afb9c286efdd763dbb8d0ae0e8dabd1f3
Eric Gomez <eric_gomez_1@hotmail.com>
2023-02-26 08:42:41 -0600

    constrain marketplace button to 200px on width

--- src/components/MarketplaceButton/index.tsx
+++ src/components/MarketplaceButton/index.tsx
@@ -1,9 +1,11 @@
 import Image from "react-bootstrap/Image";
 import React from "react";
 
+import "./styles.scss";
+
 const MarketplaceButton = (props: { href: string; src: string; }) => {
   return (
-      <a className="w-50" href={props.href}>
+      <a className="w-50 mw-200" href={props.href}>
         <Image className="img-fluid w-100" src={props.src}/>
       </a>
   )
